a251559cbb87b39d7094ccd2c510543f20151363
Hafif <hafifimammuddyn@gmail.com>
2022-09-21 17:25:12 +0700

    ♻️ use character service

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,27 +1,37 @@
-using System.Collections.Generic;
+using System.Threading.Tasks;
+using dotnet31_rpg.Dtos.Character;
 using dotnet31_rpg.Models;
+using dotnet31_rpg.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet31_rpg.Controllers
 {
-  [ApiController]
-  [Route("[controller]")]
-  public class CharacterController : ControllerBase
-  {
-    private static List<Character> characters = new List<Character> {
-      new Character(),
-      new Character { Id = 1, Name = "Sam" }
-    };
-
-    public IActionResult Get()
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
     {
-      return Ok(characters);
-    }
+        private readonly ICharacterService _characterService;
 
-    [HttpGet("{id}")]
-    public IActionResult GetSingle(int id)
-    {
-      return Ok(characters.Find(c => c.Id == id));
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCharacter(AddCharacterDto newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
     }
-  }
 }
